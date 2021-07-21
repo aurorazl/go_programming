93bb398532fe27641b67066a20ae9336f5f7a033
dragon0486 <869071540@qq.com>
2021-07-22 01:44:44 +0800

    update at 20210617

---
+++ leetcode/2. 两数相加.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+/*
+给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	carry := 0
+	sum := 0
+	var head *ListNode
+	var cur *ListNode
+	for l1 != nil || l2 != nil {
+		x := 0
+		y := 0
+		if l1 != nil {
+			x = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			y = l2.Val
+			l2 = l2.Next
+		}
+		sum = (x + y + carry)
+		sum, carry = sum%10, sum/10
+		if head == nil {
+			head = &ListNode{sum, nil}
+			cur = head
+		} else {
+			cur.Next = &ListNode{sum, nil}
+			cur = cur.Next
+		}
+	}
+	if carry > 0 {
+		cur.Next = &ListNode{carry, nil}
+	}
+	return head
+}
+func main() {
+	l1 := &ListNode{9, &ListNode{9, &ListNode{9, nil}}}
+	l2 := &ListNode{9, &ListNode{9, nil}}
+	l3 := addTwoNumbers(l1, l2)
+	for l3 != nil {
+		fmt.Println(l3.Val)
+		l3 = l3.Next
+	}
+}
---
+++ leetcode/3. 无重复字符的最长子串.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/*
+给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+*/
+
+func lengthOfLongestSubstring(s string) int {
+	storage := make(map[byte]int)
+	length := len(s)
+	maxCnt := 0
+	left := 0
+	for i := 0; i < length; i++ {
+		if _, ok := storage[s[i]]; ok {
+			left = int(math.Max(float64(storage[s[i]]+1), float64(left))) //确保重复的字符前的所有字符都不起作用了，双指针的思想
+		}
+		if i-left+1 > maxCnt {
+			maxCnt = i - left + 1
+		}
+		storage[s[i]] = i
+	}
+	return maxCnt
+}
+
+func lengthOfLongestSubstring2(s string) int {
+	storage := make(map[byte]int)
+	length := len(s)
+	maxCnt := 0
+	left := 0
+	for i := 0; i < length; i++ {
+		if i != 0 {
+			delete(storage, s[i-1])
+		}
+		for left < length && storage[s[left]] == 0 {
+			storage[s[left]]++
+			left++
+		}
+		if left-i > maxCnt {
+			maxCnt = left - i
+		}
+	}
+	return maxCnt
+}
+
+func main() {
+	fmt.Println(lengthOfLongestSubstring2("abba"))
+}
