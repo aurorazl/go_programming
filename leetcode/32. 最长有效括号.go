f85f8078544467552f870ff6653e2ca773a9a7b0
kelvin <8999>
2021-07-29 18:02:27 +0800

    upload at 20210715

--- leetcode/32. 最长有效括号.go
+++ leetcode/32. 最长有效括号.go
@@ -6,82 +6,44 @@ import "fmt"
 给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
 遍历递归，求出每种可能的长度进行比较即可。
 思路：
-	遍历字符串
-    如果是（，
-  		如果之前栈深度>0，继续遍历
-		如果深度=0，继续遍历
-	如果是），
-		如果之前栈深度>1，length+=2，更新最大值
-		如果深度=1，将length保存到list，length=0
-		如果之前栈深度=0，说明中断了，跳过当前符号，合并list，更新最大值，清空list
+2.遍历给字符串中的所有字符
+    2.1若当前字符s[index]为左括号'('，将当前字符下标index入栈（下标稍后有其他用处），处理下一字符
+    2.2若当前字符s[index]为右括号')'，判断当前栈是否为空
+        2.2.1若栈为空，则start = index + 1，处理下一字符（当前字符右括号下标不入栈）
+        2.2.2若栈不为空，则出栈（由于仅左括号入栈，则出栈元素对应的字符一定为左括号，可与当前字符右括号配对），判断栈是否为空
+            2.2.2.1若栈为空，则max = max(max, index-start+1)
+            2.2.2.2若栈不为空，则max = max(max, index-栈顶元素值)
 */
 
 func longestValidParentheses(s string) int {
+	stack := []int{}
+	start := 0
 	maxLen := 0
 	for i := 0; i < len(s); i++ {
-		if string(s[i]) == "(" {
-			curLen := calculateLen(s[i:])
-			if curLen > maxLen {
-				maxLen = curLen
-			}
-		}
-	}
-	return maxLen
-}
-
-func calculateLen(s string) int {
-	stack := 0
-	length := 0
-	tmp := []int{}
-	maxLen := 0
-	for _, i := range s {
-		if string(i) == "(" {
-			if stack > 0 {
-				tmp = append(tmp, length)
-				length = 0
-			}
-			stack++
-
+		if s[i] == '(' {
+			stack = append(stack, i)
 		} else {
-			if stack >= 1 {
-				length += 2
-				if length > maxLen {
-					maxLen = length
-				}
-				if stack == 1 {
-					sumLen := sum(tmp) + length
-					if sumLen > maxLen {
-						maxLen = sumLen
-					}
-					tmp = []int{sumLen}
-					length = 0
-				}
-				stack--
+			if len(stack) == 0 {
+				start = i + 1
 			} else {
-				sumLen := sum(tmp)
-				if sumLen > maxLen {
-					maxLen = sumLen
+				stack = stack[:len(stack)-1]
+				if len(stack) == 0 {
+					maxLen = max(maxLen, i-start+1)
+				} else {
+					maxLen = max(maxLen, i-stack[len(stack)-1])
 				}
-				tmp = []int{}
 			}
 		}
 	}
-	if len(tmp) != 0 && stack == 0 {
-		sumLen := sum(tmp)
-		if sumLen > maxLen {
-			maxLen = sumLen
-		}
-		tmp = []int{}
-	}
 	return maxLen
 }
-func sum(list []int) int {
-	val := 0
-	for _, i := range list {
-		val += i
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
-	return val
+	return b
 }
+
 func main() {
-	fmt.Println(longestValidParentheses(")()())"))
+	fmt.Println(longestValidParentheses("(()()"))
 }
---
+++ leetcode/33. 搜索旋转排序数组.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+/*
+整数数组 nums 按升序排列，数组中的值 互不相同 。
+在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，
+使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。
+例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为[4,5,6,7,0,1,2] 。
+给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回-1。
+解法：
+	二分
+*/
+
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[left] <= nums[mid] { //注意这里是等号，将更多的区域划分为升序
+			if nums[left] <= target && nums[mid] > target {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && nums[right] >= target {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(search([]int{3, 1}, 1))
+}
