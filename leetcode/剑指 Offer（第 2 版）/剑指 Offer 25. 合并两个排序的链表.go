7941ee339d7dadf5abd6068badc80f70ad761266
kelvin <8999>
2021-10-18 19:24:53 +0800

    upload at 20210913

---
+++ leetcode/剑指 Offer（第 2 版）/剑指 Offer 25. 合并两个排序的链表.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+*/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+
+}
